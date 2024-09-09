03ea22d3c3e298b6646b9c0f4d5dbb9c911a7669
C0c0-C00k13 <79659742+C0c0-C00k13@users.noreply.github.com>
2024-09-09 09:17:29 +0200

    1ere version des fichiers pour le projet
    Les classes restent encore à établir clairement.
    2 fonctions majeures présentes :
    - Une fonction permettant de récupérer les coordonnées des atomes d'un PDB;
    - Une fonction reproduisant l'algorithme de Saaf & Kuijlaars (permettant de générer une spère de N points)

---
+++ Atom.py
@@ -0,0 +1,17 @@
+
+
+class Atom:
+
+    def __init__(self, num_residu, nom_residu, num_atom, nom_atom,x, y, z):
+        self.num_atom = num_atom
+        self.num_residu = num_residu
+        self.nom_atom = nom_atom
+        self.nom_residu = nom_residu
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+if __name__ == "__main__":
+    pass
+    
---
+++ main.py
@@ -0,0 +1,10 @@
+import Bio
+from Bio.Seq import Seq
+# ----------
+from Bio.PDB import PDB 
+
+ADN = Seq("ATATCGGCTATAGCATGC")
+ADN
+
+
+# Parse PDB file
